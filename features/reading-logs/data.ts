9c274fb26640f5358669210d9ca20a56cf999a99
Anji <zam58mrp@i.softbank.jp>
2026-05-11 16:51:33 +0900

    feat: add reading log dummy data

---
+++ features/reading-logs/data.ts
@@ -0,0 +1,20 @@
+import { ReadingLog } from "./types";
+
+export const readingLogs: ReadingLog[] = [
+  {
+    id: 1,
+    bookId: 1,
+    userName: "guest1",
+    rating: 5,
+    comment: "no comment",
+    readDate: "2026-05-11",
+  },
+  {
+    id: 2,
+    bookId: 1,
+    userName: "Guest",
+    rating: 4,
+    comment: "no comment",
+    readDate: "2026-05-12",
+  },
+];
