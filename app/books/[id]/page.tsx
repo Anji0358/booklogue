52de240dba3c3eb4f7466859b3b0f35ebfe39043
Anji <zam58mrp@i.softbank.jp>
2026-05-12 13:53:28 +0900

    feat: show reading log form on book detail page

--- app/books/[id]/page.tsx
+++ app/books/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { books } from "@/features/books/data";
-import ReadingLogCard from "@/features/reading-logs/components/ReadingLogCard";
 import ReadingLogCardList from "@/features/reading-logs/components/ReadingLogCardList";
+import { ReadingLogForm } from "@/features/reading-logs/components/ReadingLogForm";
 import { readingLogs } from "@/features/reading-logs/data";
 
 type BookDetailPageProps = {
@@ -13,6 +13,7 @@ const BookDetailPage = async ({ params }: BookDetailPageProps) => {
     const { id } = await params;
 
     const bookId = Number(id);
+
     const book = books.find((book) => book.id === bookId);
 
     if (!book) {
@@ -21,15 +22,23 @@ const BookDetailPage = async ({ params }: BookDetailPageProps) => {
     const bookReadingLogs = readingLogs.filter((readingLog) => readingLog.bookId === bookId);
 
     return (
-        <>
-            <h1>{book.title}</h1>
-            <p>著者：{book.author}</p>
-            <p>カテゴリ：{book.category}</p>
-            <p>出版年：{book.publishedDate}</p>
-            <p>{book.description}</p>
+        <main>
+
+            <section>
+                <h1>{book.title}</h1>
+                <p>著者：{book.author}</p>
+                <p>カテゴリ：{book.category}</p>
+                <p>出版年：{book.publishedDate}</p>
+                <p>{book.description}</p>
+            </section>
+
+            <section>
+                <h2>読書ログを投稿する</h2>
+                <ReadingLogForm />
+            </section>
 
             <ReadingLogCardList readingLogs={bookReadingLogs} />
-        </>
+        </main>
     )
 }
 
